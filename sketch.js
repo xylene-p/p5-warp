aeb339bba074a82d0cbd309a7791b38e1c8ddaf9
Kat Perez <xylene.dev@gmail.com>
2024-06-01 14:50:36 -0400

    init

---
+++ sketch.js
@@ -0,0 +1,8 @@
+function setup() {
+  createCanvas(400, 400); // Create a canvas
+}
+
+function draw() {
+  background(220); // Set the background color
+  ellipse(mouseX, mouseY, 50, 50); // Draw a circle at the mouse position
+}
